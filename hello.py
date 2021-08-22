7fee27ce027cd203130d403e32c584ea70368c8e
Sakkadas <eildos95@gmail.com>
2021-08-23 02:12:07 +0300

    create custom error pages (404, 500)

--- hello.py
+++ hello.py
@@ -18,3 +18,17 @@ def index():
 @app.route('/user/<name>')
 def user(name):
     return render_template('user.html', user_name=name)
+
+
+# Create Custom Error Pages
+
+# Invalid URl
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("404.html"), 404
+
+
+# Internal server Error
+@app.errorhandler(500)
+def page_not_found(e):
+    return render_template("500.html"), 500
