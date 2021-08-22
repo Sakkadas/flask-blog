6525358d4af46699c2ad8ab22094eac189090f5b
Sakkadas <eildos95@gmail.com>
2021-08-23 01:56:15 +0300

    test features with jinja

--- hello.py
+++ hello.py
@@ -7,10 +7,14 @@ app = Flask(__name__)
 # Create a route decorator
 @app.route('/')
 def index():
-    return render_template("index.html")
+    first_name = "Ilya"
+    stuff = "This is bold text"
+
+    favorite_pizza = ["Pepperoni", "Cheese", "Mushrooms", 41]
+    return render_template("index.html", first_name=first_name, stuff=stuff, favorite_pizza=favorite_pizza)
 
 
 # localhost:5000/user/ilya
 @app.route('/user/<name>')
 def user(name):
-    return f"Hello {name}!"
+    return render_template('user.html', user_name=name)
