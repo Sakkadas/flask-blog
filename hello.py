dc1564ecbb1ae24e2b258bee3ab1be33286c4b36
Sakkadas <eildos95@gmail.com>
2021-09-02 17:09:20 +0300

    import flash

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, flash
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
@@ -53,4 +53,5 @@ def name():
     if form.validate_on_submit():
         name = form.name.data
         form.name.data = ''
+        flash("Form Submitted Successfully")
     return render_template("name.html", name=name, form=form)
