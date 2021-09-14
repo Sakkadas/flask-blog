5fe44feac790a177315eebc5911ed3d95d71b4d5
Ilya Nesterov <eildos95@gmail.com>
2021-09-15 01:00:28 +0300

    implement passwords hashing

--- hello.py
+++ hello.py
@@ -9,12 +9,14 @@ from datetime import datetime
 from flaskext.mysql import MySQL
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from werkzeug.security import generate_password_hash, check_password_hash
 
 # load dotenv
 load_dotenv()
 
 # Create a Flask Instance
 app = Flask(__name__)
+
 
 # SECRET KEY
 app.config['SECRET_KEY'] = "placeholder"
@@ -44,6 +46,20 @@ class Users(db.Model):
     email = db.Column(db.String(120), nullable=False, unique=True)
     favorite_color = db.Column(db.String(120))
     date_added = db.Column(db.DateTime, default=datetime.utcnow)
+
+    # Passwords hashing
+    password_hash = db.Column(db.String(128))
+
+    @property
+    def password(self):
+        raise AttributeError('password is not a readeble attribute!')
+
+    @password.setter
+    def password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def verify_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
     # Create __str__
     def __repr__(self):
